8cec0339a64b02db5c006838d413953fdfc7b9a9
Raul Romo <rromomxdev@gmail.com>
2020-05-17 16:16:12 -0500

    a - mongo connection

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const polka = require('polka');
 const PORT = process.env.PORT || 5000;
+const uriMongo = process.env.DRIVER;
+const MongoClient = require('mongodb').MongoClient;
+let mongodb;
 
-polka()
-  .get('/', (req, res) => {
-    res.end('Hello world!');
-  })
-  .listen(PORT, err => {
+MongoClient.connect(uriMongo, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+}, (err, database) => {
+  if (err) return console.error(err);
+  console.log('Connected to Database');
+  mongodb = database;
+});
+
+const app = polka();
+
+app.get('/', (req, res) => {
+  res.end('Hello world!');
+});
+
+app.get('/users', (req, res) => {
+  const data = mongodb.db('testdb');
+
+  data.collection('colltest')
+    .findOne()
+    .then((result)=>{
+      console.log(result)
+      res.end(JSON.stringify(result))
+    })
+    .catch(err => console.log(err))
+  
+});
+
+app.listen(PORT, err => {
     if (err) throw err;
     console.log(`> Running on localhost:${PORT}`);
   });
