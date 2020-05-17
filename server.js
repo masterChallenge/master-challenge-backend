29f97b0e6e0cd266847a4055e4ec16bc99d98e6a
Raul Romo <rromomxdev@gmail.com>
2020-05-17 15:41:43 -0500

    u - server port config

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const polka = require('polka');
+const PORT = process.env.PORT || 5000;
 
 polka()
   .get('/', (req, res) => {
     res.end('Hello world!');
   })
-  .listen(3000, err => {
+  .listen(PORT, err => {
     if (err) throw err;
-    console.log(`> Running on localhost:3000`);
+    console.log(`> Running on localhost:${PORT}`);
   });
