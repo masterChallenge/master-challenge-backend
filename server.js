62174ab39db5c664c58fc5d60880c2b7e9948daa
Raul Romo <rromomxdev@gmail.com>
2021-03-21 11:56:14 -0600

    remove dotenv

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const polka = require('polka');
-require('dotenv').config();
+//require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const uriMongo = process.env.DRIVER;
 const MongoClient = require('mongodb').MongoClient;
